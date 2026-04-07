bc8387c8a6b9a7ae2ce5a2e790cd0fa4434b2875
paulinet <paulinet@mac.home>
2026-04-07 16:22:28 +0200

    feat: call backend using fetch
    - fetch is a web api method from window object
    
    - fetch return an promise manageable using then
    
    - use .json() on the object fetched to get the data
    
    - .json() also return a promise so use then a second time

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,17 @@ import { products } from "../../starting-code/data/products"
 import CheckmarkImage from "../assets/images/icons/checkmark.png"
 
 export default function HomePage() {
+    fetch("http://localhost:3000/api/products")
+        .then(
+            (response) => {
+                return response.json()
+            }
+        ).then(
+            (data) => {
+                console.log(data)
+            }
+        )
+
     return (
         <>
             <link rel="icon" type="image/svg+xml" href="/images/home-favicon.png" />
