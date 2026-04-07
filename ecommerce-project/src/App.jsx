5c0f50d30fbfd45f6678d631b289f3fd9ecef977
paulinet <paulinet@mac.home>
2026-04-07 10:24:01 +0200

    feat: move CheckoutPage in react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './components/HomePage'
 import './App.css'
+import CheckoutPage from './components/CheckoutPage'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       />
       <Route
         path="/checkout"
-        element={<div> test </div>}
+        element={<CheckoutPage />}
       />
     </Routes>
   )
